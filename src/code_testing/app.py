996ebe026844355e9522c179b64ca8ab53cbe83c
Oytizzo <oytizzo@gmail.com>
2021-10-19 23:48:03 +0600

    Bugfix

--- src/code_testing/app.py
+++ src/code_testing/app.py
@@ -8,4 +8,4 @@ def average(L):
 if __name__ == "__main__":
     L = [1, 2, 3, 4, 5]
     expected_result = 3.0
-    assert  expected_result == average(L), "average() produced incorrect result"
+    assert expected_result == average(L), "average() produced incorrect result"

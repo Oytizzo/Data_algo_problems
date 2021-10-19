2bd4d875e32c34ebf1bb7d79f73199a60a7654c3
Oytizzo <oytizzo@gmail.com>
2021-10-20 02:54:07 +0600

    Unit test using pytest

--- src/code_testing/app.py
+++ src/code_testing/app.py
@@ -5,7 +5,5 @@ def average(L):
     return sum(L)/len(L)
 
 
-if __name__ == "__main__":
-    L = [1, 2, 3, 4, 5]
-    expected_result = 3.0
-    assert expected_result == average(L), "average() produced incorrect result"
+def test_average():
+    assert 3.0 == average([1, 2, 3, 4, 5])

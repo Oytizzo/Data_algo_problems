b5c3adb25c9df728c3b71c7ffd2b1fa088c2b04a
Oytizzo <oytizzo@gmail.com>
2021-10-29 23:59:12 +0600

    Add functions

--- src/linked_list/doubly_linked_list.py
+++ src/linked_list/doubly_linked_list.py
@@ -6,3 +6,33 @@ class Node:
 
     def __repr__(self):
         return repr(self.data)
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = Node()
+
+    def __repr__(self):
+        nodes = []
+        current_node = self.head.next
+
+        while current_node:
+            nodes.append(repr(current_node))
+            current_node = current_node.next
+
+        return ','.join(nodes)
+
+    def append(self, data):
+        pass
+
+    def prepend(self, data):
+        pass
+
+    def search(self, item):
+        pass
+
+    def remove_node(self, node):
+        pass
+
+    def remove(self, item):
+        pass
